3e05c829222bfad61e69ff33f7901559dfbc8014
cherriod <cherriod@gmail.com>
2015-04-14 14:49:07 +0600

    Init commit

---
+++ PasswordGenerator/Password.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    public class Password
+    {
+        private bool pronounceable;
+        private PasswordConditions passwordConditions;
+        private int quantities;
+/*
+        public string Password
+        {
+            get { return getPassword(); }
+        }
+*/
+
+        public string GetPassword()
+        {
+            return "OK";
+        }
+
+        public Password(int quantities, PasswordConditions passwordConditions, bool pronounceable = true)
+        {
+            this.quantities = quantities;
+            this.passwordConditions = passwordConditions;
+            this.pronounceable = pronounceable;
+        }
+    }
+}
---
+++ PasswordGenerator/PasswordConditions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    public struct PasswordConditions
+    {
+        private int valueVowels;
+        private int valueConsonant;
+        private int valueNumbers;
+        private int valueSimbols;
+        
+
+        public PasswordConditions(int valueVowels, int valueConsonant,int valueNumbers,  int valueSimbols)
+        {
+            this.valueVowels = valueVowels;
+            this.valueConsonant = valueConsonant;
+            this.valueNumbers = valueNumbers;
+            this.valueSimbols = valueSimbols;
+        }
+    }
+}
